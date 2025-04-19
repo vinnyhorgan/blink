be266fa40d39986fab8b6500e865e61848768ee2
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2025-04-19 17:11:04 +0200

    change some style

--- src/main.zig
+++ src/main.zig
@@ -32,6 +32,26 @@ fn draw(window: ?*c.GLFWwindow) void {
 
     _ = c.ImGui_DockSpaceOverViewport();
 
+    _ = c.ImGui_BeginMainMenuBar();
+
+    if (c.ImGui_BeginMenu("File")) {
+        c.ImGui_EndMenu();
+    }
+
+    if (c.ImGui_BeginMenu("Edit")) {
+        c.ImGui_EndMenu();
+    }
+
+    if (c.ImGui_BeginMenu("View")) {
+        c.ImGui_EndMenu();
+    }
+
+    if (c.ImGui_BeginMenu("Help")) {
+        c.ImGui_EndMenu();
+    }
+
+    c.ImGui_EndMainMenuBar();
+
     c.ImGui_ShowDemoWindow(null);
 
     global_console.render();
@@ -66,6 +86,13 @@ fn color(r: f32, g: f32, b: f32, a: f32) c.ImVec4 {
     return v;
 }
 
+fn vec2(x: f32, y: f32) c.ImVec2 {
+    var v = c.ImVec2{};
+    v.x = x;
+    v.y = y;
+    return v;
+}
+
 pub fn main() !void {
     _ = c.glfwSetErrorCallback(errorCallback);
 
@@ -79,7 +106,7 @@ pub fn main() !void {
 
     c.glfwWindowHint(c.GLFW_VISIBLE, c.GLFW_FALSE);
 
-    const window = c.glfwCreateWindow(960, 540, "blink", null, null);
+    const window = c.glfwCreateWindow(1280, 720, "blink", null, null);
     if (window == null) {
         return;
     }
@@ -100,49 +127,19 @@ pub fn main() !void {
 
     const style = c.ImGui_GetStyle();
 
-    style.*.Colors[c.ImGuiCol_Text] = color(0.0, 0.0, 0.0, 1.0);
+    // final style
+    style.*.WindowMenuButtonPosition = c.ImGuiDir_None;
+    style.*.FramePadding = vec2(6.0, 6.0);
+    style.*.ScrollbarRounding = 0.0;
+    style.*.TabRounding = 0.0;
+    style.*.TabBarBorderSize = 0.0;
+    style.*.ChildRounding = 4.0;
+    style.*.FrameRounding = 4.0;
+    style.*.PopupRounding = 4.0;
+    style.*.ScrollbarRounding = 4.0;
+    style.*.GrabRounding = 2.0;
 
-    style.*.Colors[c.ImGuiCol_Text] = color(1.00, 1.00, 1.00, 1.00);
-    style.*.Colors[c.ImGuiCol_TextDisabled] = color(0.50, 0.50, 0.50, 1.00);
-    style.*.Colors[c.ImGuiCol_WindowBg] = color(0.06, 0.06, 0.06, 0.94);
-    style.*.Colors[c.ImGuiCol_ChildBg] = color(1.00, 1.00, 1.00, 0.00);
-    style.*.Colors[c.ImGuiCol_PopupBg] = color(0.08, 0.08, 0.08, 0.94);
-    style.*.Colors[c.ImGuiCol_Border] = color(0.43, 0.43, 0.50, 0.50);
-    style.*.Colors[c.ImGuiCol_BorderShadow] = color(0.00, 0.00, 0.00, 0.00);
-    style.*.Colors[c.ImGuiCol_FrameBg] = color(0.20, 0.21, 0.22, 0.54);
-    style.*.Colors[c.ImGuiCol_FrameBgHovered] = color(0.40, 0.40, 0.40, 0.40);
-    style.*.Colors[c.ImGuiCol_FrameBgActive] = color(0.18, 0.18, 0.18, 0.67);
-    style.*.Colors[c.ImGuiCol_TitleBg] = color(0.04, 0.04, 0.04, 1.00);
-    style.*.Colors[c.ImGuiCol_TitleBgActive] = color(0.29, 0.29, 0.29, 1.00);
-    style.*.Colors[c.ImGuiCol_TitleBgCollapsed] = color(0.00, 0.00, 0.00, 0.51);
-    style.*.Colors[c.ImGuiCol_MenuBarBg] = color(0.14, 0.14, 0.14, 1.00);
-    style.*.Colors[c.ImGuiCol_ScrollbarBg] = color(0.02, 0.02, 0.02, 0.53);
-    style.*.Colors[c.ImGuiCol_ScrollbarGrab] = color(0.31, 0.31, 0.31, 1.00);
-    style.*.Colors[c.ImGuiCol_ScrollbarGrabHovered] = color(0.41, 0.41, 0.41, 1.00);
-    style.*.Colors[c.ImGuiCol_ScrollbarGrabActive] = color(0.51, 0.51, 0.51, 1.00);
-    style.*.Colors[c.ImGuiCol_CheckMark] = color(0.94, 0.94, 0.94, 1.00);
-    style.*.Colors[c.ImGuiCol_SliderGrab] = color(0.51, 0.51, 0.51, 1.00);
-    style.*.Colors[c.ImGuiCol_SliderGrabActive] = color(0.86, 0.86, 0.86, 1.00);
-    style.*.Colors[c.ImGuiCol_Button] = color(0.44, 0.44, 0.44, 0.40);
-    style.*.Colors[c.ImGuiCol_ButtonHovered] = color(0.46, 0.47, 0.48, 1.00);
-    style.*.Colors[c.ImGuiCol_ButtonActive] = color(0.42, 0.42, 0.42, 1.00);
-    style.*.Colors[c.ImGuiCol_Header] = color(0.70, 0.70, 0.70, 0.31);
-    style.*.Colors[c.ImGuiCol_HeaderHovered] = color(0.70, 0.70, 0.70, 0.80);
-    style.*.Colors[c.ImGuiCol_HeaderActive] = color(0.48, 0.50, 0.52, 1.00);
-    style.*.Colors[c.ImGuiCol_Separator] = color(0.43, 0.43, 0.50, 0.50);
-    style.*.Colors[c.ImGuiCol_SeparatorHovered] = color(0.72, 0.72, 0.72, 0.78);
-    style.*.Colors[c.ImGuiCol_SeparatorActive] = color(0.51, 0.51, 0.51, 1.00);
-    style.*.Colors[c.ImGuiCol_ResizeGrip] = color(0.91, 0.91, 0.91, 0.25);
-    style.*.Colors[c.ImGuiCol_ResizeGripHovered] = color(0.81, 0.81, 0.81, 0.67);
-    style.*.Colors[c.ImGuiCol_ResizeGripActive] = color(0.46, 0.46, 0.46, 0.95);
-    style.*.Colors[c.ImGuiCol_PlotLines] = color(0.61, 0.61, 0.61, 1.00);
-    style.*.Colors[c.ImGuiCol_PlotLinesHovered] = color(1.00, 0.43, 0.35, 1.00);
-    style.*.Colors[c.ImGuiCol_PlotHistogram] = color(0.73, 0.60, 0.15, 1.00);
-    style.*.Colors[c.ImGuiCol_PlotHistogramHovered] = color(1.00, 0.60, 0.00, 1.00);
-    style.*.Colors[c.ImGuiCol_TextSelectedBg] = color(0.87, 0.87, 0.87, 0.35);
-    style.*.Colors[c.ImGuiCol_DragDropTarget] = color(1.00, 1.00, 0.00, 0.90);
-    style.*.Colors[c.ImGuiCol_NavHighlight] = color(0.60, 0.60, 0.60, 1.00);
-    style.*.Colors[c.ImGuiCol_NavWindowingHighlight] = color(1.00, 1.00, 1.00, 0.70);
+    style.*.Colors[c.ImGuiCol_ResizeGrip] = color(0.00, 0.00, 0.00, 0.00);
 
     io = c.ImGui_GetIO();
     io.*.ConfigFlags |= c.ImGuiConfigFlags_DockingEnable;
